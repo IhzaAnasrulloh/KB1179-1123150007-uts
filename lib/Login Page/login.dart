499791a08d623aed83fd94fc4de81b84fc1a0503
Ihza Anasrulloh <ihzaanasrulloh55@gmail.com>
2025-11-07 12:33:19 +0700

    menambahkan tulisan selamat datang dan di wrap ke center

--- lib/Login Page/login.dart
+++ lib/Login Page/login.dart
@@ -5,23 +5,41 @@ class login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(20.0),
-          child: const Column(
-            children: [
+     return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
               SizedBox(height: 10,),
               Icon(
                 Icons.lock_outline,
                 size: 80,
                 color: Colors.deepPurple,
               ),
-              SizedBox(height: 10,)
+              SizedBox(height: 10,),
+
+              const Text(
+                "Selamat Datang JAWA",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepPurple,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                "Masuk untuk melanjutkan ke akun jawa anda",
+                style: TextStyle(color: Colors.grey, fontSize: 14),
+              ),
+              const SizedBox(height: 40),
             ],
           ),
         ),
       ),
+      )
     );
   }
 }
